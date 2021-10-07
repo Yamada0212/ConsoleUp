6bf9fcf658423340d87ceada2ca55ae98eba5ceb
ヤマダ　ガイト <212E007@GAME.LOCAL>
2021-10-07 17:19:06 +0900

    プロジェクト ファイルを追加します。

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleUp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
